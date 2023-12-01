ca8824fa4a35f91238fce41f2a78544fd71221f2
Gabriel Castro <gabrieltoby@gmail.com>
2023-12-01 12:46:30 -0300

    adicionada nova moto

--- src/js/motos.js
+++ src/js/motos.js
@@ -1,9 +1,74 @@
 let motosJson = [
-    {id:0, model:'Harley-Davidson 883 R.', year: '2012', price: '34900', kms: '', description: 'Pastilhas de freio novas | Alarme Original HD | Documentos em dia | Aceitamos moto na troca | Também pegamos carro', sell: 'no'},
-    {id:1, model:'Yamaha R3', year: '2021', price: '26900', kms: '21 mil km rodados', description: 'Toda original - IMPECÁVEL | Suspensão invertida | Pneus novos | Relação nova | Painel digital | Placa i | Manual e chave reserva | Documentos em dia', sell: 'no'},
-    {id:2, model: 'HD Fat Boy 1600 cc', year: '2009', price: '54900', kms: '38 mil km rodados', description: 'Pneus novos | Escapamento Scremin Eagle | Lentes de piscas fumê e sinaleira de led | Valor abaixo da FIPE', sell: 'no'},
-    {id:3, model: 'HD Heritage 1450 cc', year: '2006', price: '62900', kms: '36 mil km rodados', description: 'Lorem ipsum dolor sit amet consectetur. Elementum consequat id et donec orci.', sell: 'yes'},
-    {id:4, model: 'Fat Boy 1600 cc', year: '2010', price: '56500', kms: '48 mil km rodados', description: 'Pneus novos | Manual e chave reserva | Valor abaixo da FIPE', sell: 'no'},
-    {id:5, model: 'Suzuki Haojue Chopper Road 150 cc', year: '2022', price: '10900', kms: '23 mil km rodados', description: 'Único dono | Toda revisada | Pneus novos | Relação trocada', sell: 'no'},
-    {id:6, model: 'Honda Biz 125 cc', year: '2017', price: '12500', kms: '43 mil km rodados', description: 'Único dono | Bateria nova | Freio a disco | Rodas de liga | Partida elétrica', sell: 'no'},
+	{
+        id: 0,
+        model: 'Harley-Davidson 883 R.',
+        year: '2012',
+        price: '34900',
+        kms: '',
+        description: 'Pastilhas de freio novas | Alarme Original HD | Documentos em dia | Aceitamos moto na troca | Também pegamos carro',
+        sell: 'no'
+    },
+    {
+        id: 1,
+        model: 'Yamaha R3',
+        year: '2021',
+        price: '26900',
+        kms: '21 mil km rodados',
+        description: 'Toda original - IMPECÁVEL | Suspensão invertida | Pneus novos | Relação nova | Painel digital | Placa i | Manual e chave reserva | Documentos em dia',
+        sell: 'no'
+    },
+    {
+        id: 2,
+        model: 'HD Fat Boy 1600 cc',
+        year: '2009',
+        price: '54900',
+        kms: '38 mil km rodados',
+        description: 'Pneus novos | Escapamento Scremin Eagle | Lentes de piscas fumê e sinaleira de led | Valor abaixo da FIPE',
+        sell: 'no'
+    },
+    {
+        id: 3,
+        model: 'HD Heritage 1450 cc',
+        year: '2006',
+        price: '62900',
+        kms: '36 mil km rodados',
+        description: 'Lorem ipsum dolor sit amet consectetur. Elementum consequat id et donec orci.',
+        sell: 'yes'
+    },
+    {
+        id: 4,
+        model: 'Fat Boy 1600 cc',
+        year: '2010',
+        price: '56500',
+        kms: '48 mil km rodados',
+        description: 'Pneus novos | Manual e chave reserva | Valor abaixo da FIPE',
+        sell: 'no'
+    },
+    {
+        id: 5,
+        model: 'Suzuki Haojue Chopper Road 150 cc',
+        year: '2022',
+        price: '10900',
+        kms: '23 mil km rodados',
+        description: 'Único dono | Toda revisada | Pneus novos | Relação trocada',
+        sell: 'no'
+    },
+    {
+        id: 6,
+        model: 'Honda Biz 125 cc',
+        year: '2017',
+        price: '12500',
+        kms: '43 mil km rodados',
+        description: 'Único dono | Bateria nova | Freio a disco | Rodas de liga | Partida elétrica',
+        sell: 'no'
+    },
+    {
+        id: 7,
+        model: 'Harley Davidson Sportster 883R',
+        year: '2008',
+        price: '31900',
+        kms: '69 mil km rodados',
+        description: 'Valor agregado de customização acima de R$6.000 | Banco personalizado | Valor abaixo da FIPE',
+        sell: 'no'
+    },
 ];
