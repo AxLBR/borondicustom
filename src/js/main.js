8b65a5a6ee0e149b1560c117a63ad3b9f1fa8bc2
Gabriel Castro <gabrieltoby@gmail.com>
2024-08-21 19:25:12 -0300

    feat: updated to version 3

--- src/js/main.js
+++ src/js/main.js
@@ -14,14 +14,44 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   dropdownMenu.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function() {
-        dropdownMenu.classList.remove('open');
-        isDropdownOpen = false;
-    });
-});
+      link.addEventListener('click', function() {
+          dropdownMenu.classList.remove('open');
+          isDropdownOpen = false;
+      });
+  });
 });
 
-//Abre moto na modal
+// Função para pré-carregar imagens
+function preloadImagesForMoto(motoId) {
+  let index = 1;
+  let imgSrc = `./src/images/motos/${motoId}/${index}.webp`;
+
+  const checkImages = () => {
+      const img = new Image();
+      img.src = imgSrc;
+      img.onload = () => {
+          // Incrementa o índice para verificar a próxima imagem
+          index++;
+          imgSrc = `./src/images/motos/${motoId}/${index}.webp`;
+          checkImages(); // Continua carregando as próximas imagens
+      };
+      img.onerror = () => {
+          // Finaliza o loop quando não há mais imagens
+          console.log(`Pré-carregamento de imagens da moto ${motoId} concluído.`);
+      };
+  };
+
+  checkImages();
+}
+
+// Pré-carregar imagens ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+  reverseMotosJson.forEach(moto => {
+      preloadImagesForMoto(moto.id);
+  });
+});
+
+// Abre moto na modal
 function openItem(key) {
   let fullScreen = document.querySelector(".fullScreen");
   let fullScreenContainer = document.querySelector(".fullScreenContainer");
@@ -29,23 +59,23 @@ function openItem(key) {
   let moto;
 
   reverseMotosJson.map((item) => {
-    if (item.id == key) {
-      moto = item;
-    }
+      if (item.id == key) {
+          moto = item;
+      }
   });
 
   let newPrice = moto.price
-    .toString()
-    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+      .toString()
+      .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
 
   document.querySelector(".motoHead p").innerHTML =
-    moto.model + " (" + moto.year + ")";
+      moto.model + " (" + moto.year + ")";
 
   if (moto.sell == "yes") {
-    document.querySelector(".motoHead button").textContent = "VENDIDO";
-    document.querySelector(".motoHead button").className = "vendido";
+      document.querySelector(".motoHead button").textContent = "VENDIDO";
+      document.querySelector(".motoHead button").className = "vendido";
   } else {
-    document.querySelector(".motoHead button").textContent = "R$" + newPrice;
+      document.querySelector(".motoHead button").textContent = "R$" + newPrice;
   }
 
   document.querySelector(".motoOther").innerHTML = moto.kms;
@@ -53,70 +83,66 @@ function openItem(key) {
   document.querySelector(".motoPayment").innerHTML = moto.credit;
   document.querySelector(".proposalButton").href = "https://wa.me/5191353965?text=Olá, gostaria de informações sobre a " + moto.model + ' (' + moto.year + ')';
 
-  function imageExists(src) {
-    return fetch(src, { method: "HEAD" })
-      .then((res) => res.ok)
-      .catch(() => false);
-  }
-
-  // Verifica e insere quantas imagens houver na pasta da moto
   const imageElements = [];
   let index = 1;
   let imgSrc = `./src/images/motos/${key}/${index}.webp`;
 
-  const checkImages = async () => {
-    while (await imageExists(imgSrc)) {
-      imageElements.push(
-        `<div class="carrosselFullscreen"><img src="${imgSrc}" title="${moto.model}" alt="Foto ${index} da ${moto.model}"/></div>`
-      );
-      index++;
-      imgSrc = `./src/images/motos/${key}/${index}.webp`;
-    }
+  const motoPicturesElement = document.querySelector(".motoPictures");
 
-    const motoPicturesElement = document.querySelector(".motoPictures");
-    motoPicturesElement.innerHTML = imageElements.join("");
+  // Carregar todas as imagens para o carrossel
+  const addImagesToCarousel = () => {
+      imageExists(imgSrc, (exists) => {
+          if (exists) {
+              imageElements.push(
+                  `<div class="carrosselFullscreen"><img src="${imgSrc}" title="${moto.model}" alt="Foto ${index} da ${moto.model}"/></div>`
+              );
+              index++;
+              imgSrc = `./src/images/motos/${key}/${index}.webp`;
+              addImagesToCarousel();
+          } else {
+              motoPicturesElement.innerHTML = imageElements.join("");
 
-    // Inicializa o carrossel após as imagens serem inseridas
-    if (imageElements.length > 0) {
-      $(motoPicturesElement).slick({
-        dots: true,
-        infinite: true,
-        rows: 1,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-
-        responsive: [
-          {
-            breakpoint: 1660,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 1015,
-            settings: {
-              rows: 1,
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 802,
-            settings: {
-              rows: 1,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-        ],
+              if (imageElements.length > 0) {
+                  $(motoPicturesElement).slick({
+                      dots: true,
+                      infinite: true,
+                      rows: 1,
+                      slidesToShow: 3,
+                      slidesToScroll: 3,
+                      responsive: [
+                          {
+                              breakpoint: 1660,
+                              settings: {
+                                  slidesToShow: 2,
+                                  slidesToScroll: 2,
+                              },
+                          },
+                          {
+                              breakpoint: 1015,
+                              settings: {
+                                  rows: 1,
+                                  slidesToShow: 2,
+                                  slidesToScroll: 2,
+                              },
+                          },
+                          {
+                              breakpoint: 802,
+                              settings: {
+                                  rows: 1,
+                                  slidesToShow: 1,
+                                  slidesToScroll: 1,
+                              },
+                          },
+                      ],
+                  });
+              } else {
+                  console.error("Nenhuma imagem encontrada para inicializar o slick.");
+              }
+          }
       });
-    } else {
-      console.error("Nenhuma imagem encontrada para inicializar o slick.");
-    }
   };
 
-  checkImages();
+  addImagesToCarousel();
 
   $("html").css({ overflowY: "hidden" });
   fullScreen.style.display = "block";
@@ -124,7 +150,15 @@ function openItem(key) {
   main.style.filter = "blur(1px)";
 }
 
-//Fecha modal
+// Função para verificar a existência da imagem
+function imageExists(src, callback) {
+  const img = new Image();
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+  img.src = src;
+}
+
+// Fecha modal
 function closeModal() {
   let fullScreen = document.querySelector(".fullScreen");
   let main = document.querySelector("main");
@@ -143,7 +177,7 @@ function closeModal() {
   main.style.filter = "";
 }
 
-//Adiciona ano nos rodapés
+// Adiciona ano nos rodapés
 document.querySelectorAll(".year").forEach((rodape) => {
   const d = new Date();
   let year = d.getFullYear();
@@ -151,63 +185,63 @@ document.querySelectorAll(".year").forEach((rodape) => {
   rodape.innerHTML = year;
 });
 
-//Revela objetos conforme scroll da página
+// Revela objetos conforme scroll da página
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
 
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+      if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+      } else {
+          reveals[i].classList.remove("active");
+      }
   }
 }
 
 window.addEventListener("scroll", reveal);
 
-//Botões de navegação
+// Botões de navegação
 $(document).ready(function () {
   const menuBtn = $(".scroll");
   menuBtn.click(() => {
-    setTimeout(() => {
-      let downArrowButton = document.querySelector(".downArrow");
-      let upArrowButton = document.querySelector(".upArrow");
+      setTimeout(() => {
+          let downArrowButton = document.querySelector(".downArrow");
+          let upArrowButton = document.querySelector(".upArrow");
 
-      if (downArrowButton.style.display != "none") {
-        downArrowButton.style.display = "none";
-        upArrowButton.style.display = "block";
-      } else {
-        downArrowButton.style.display = "block";
-        upArrowButton.style.display = "none";
-      }
+          if (downArrowButton.style.display != "none") {
+              downArrowButton.style.display = "none";
+              upArrowButton.style.display = "block";
+          } else {
+              downArrowButton.style.display = "block";
+              upArrowButton.style.display = "none";
+          }
 
-      removeHash();
-    }, 5);
+          removeHash();
+      }, 5);
   });
 });
 
-//Da o scroll de forma suave
+// Dá o scroll de forma suave
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+      e.preventDefault();
 
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+      });
   });
 });
 
-//Remove o # do link da página
+// Remove o # do link da página
 function removeHash() {
   history.pushState(
-    "",
-    document.title,
-    window.location.pathname + window.location.search
+      "",
+      document.title,
+      window.location.pathname + window.location.search
   );
 }
 
@@ -215,7 +249,7 @@ window.onload = function () {
   removeHash();
 };
 
-//Reseta medidas da página para o mobile
+// Reseta medidas da página para o mobile
 function resetHeight() {
   document.body.style.height = window.innerHeight + "px";
 }
@@ -223,13 +257,13 @@ function resetHeight() {
 window.addEventListener("resize", resetHeight);
 resetHeight();
 
-//Previne que o usuario dê zoom na página pelo scroll do mouse
+// Previne que o usuário dê zoom na página pelo scroll do mouse
 document.addEventListener(
   "wheel",
   function touchHandler(e) {
-    if (e.ctrlKey) {
-      e.preventDefault();
-    }
+      if (e.ctrlKey) {
+          e.preventDefault();
+      }
   },
   { passive: false }
 );
